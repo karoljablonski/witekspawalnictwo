65a123870d3dfe0bb7937d5986f8d9bfbc81a1ac
Karol Jabłoński <karoljablonski373@gmail.com>
2021-07-18 00:43:34 +0200

    html: little fix, css: 100% index done, some fixin need when decided on what to display on is-active burger. js: simple toggle func

--- javascript/index.js
+++ javascript/index.js
@@ -14,15 +14,23 @@ window.addEventListener("scroll", function(){
 
 //hamburger: pojawianie się na zmianę wielkości okna; klik na burger powoduje odpalenie nav;
 const burger = document.querySelector('button.hamburger');
-const navigation = document.querySelector('ul');
-const smartphone590width = 590;
+const navigation = document.querySelector('div.main_nav_wrapper>ul');
 
+// burger.style.display = "block";
+// navigation.classList.add("unshow_ul")
+//zadaniem burgera, jest na klik -> pokazać ul i zmienic sie na X, na kolejny -> unshow na ul i zmienic sie na burger. w momencie, gdy burger znika ,czyli szerokosc okna wieksza niz 550px, wszystko wraca do normy, czyli: pojawia sie normalnie ul, znika burger.
 
 burger.addEventListener("click", ()=>{
-    console.log("clicked button");
+    navigation.classList.toggle("show_ul");
     burger.classList.toggle("is-active");
-    navigation.classList.toggle("ul_show");
 })
+
+
+// burger.addEventListener("click", ()=>{
+//     console.log("clicked button");
+//     burger.classList.toggle("is-active");
+//     navigation.classList.toggle("ul_show");
+// })
 
 // window.addEventListener("resize", function(){
 //     if(window.innerWidth <= smartphone590width){
